7004777978528f89f39b6b9b4556924a46d9232c
Jarred Green <astrojarred@gmail.com>
2021-01-08 18:37:17 +0100

    Add complete scraping functionality.

--- messing.py
+++ messing.py
@@ -1,51 +1,124 @@
 import ipuz
+import json
 import requests
+import datetime
 from bs4 import BeautifulSoup
 
-URL = "https://www.theguardian.com/crosswords/accessible/quiptic/1103"
 
-page = requests.get(URL)
+# make a blank puzzle
+def make_blank_puzzle(width, height):
+    puzzle = []
+    for row in range(height):
+        puzzle.append(["#"] * width)
 
-soup = BeautifulSoup(page.content, "html.parser")
+    return puzzle
 
-# notes:
-# <div class="crossword__accessible-data">
 
-# Extract clues
-# =============
+# get the solution from the entries
+def get_solution(width, height, data):
 
-# get the across and down clues class
-across = soup.find("div", class_="crossword__clues--across").find_all(
-    "li", class_="crossword__clue"
-)
-down = soup.find("div", class_="crossword__clues--down").find_all(
-    "li", class_="crossword__clue"
-)
+    blank_puzzle = make_blank_puzzle(15, 15)
 
-# create dictionaries for the clues
-clues = dict()
-clues["across"] = []
-clues["down"] = []
+    # fill in across answers to solution
+    for clue in data["entries"]:
+        x = clue["position"]["x"]
+        y = clue["position"]["y"]
+        length = clue["length"]
+        solution = clue["solution"]
+        if clue["direction"] == "across":
+            blank_puzzle[y][x : x + length] = list(solution)
+        elif clue["direction"] == "down":
+            for index, row in enumerate(range(y, y + length)):
+                blank_puzzle[row][x] = solution[index]
 
-# extract the clues from the divs to a list
-for clue in across:
-    value = int(clue.get_attribute_list("value")[0])
-    close_parenthesis = clue.get_text().find(")") + 2
-    text = clue.get_text()[close_parenthesis:]
-    clues["across"].append([value, text])
+    return blank_puzzle
 
-for clue in down:
-    value = int(clue.get_attribute_list("value")[0])
-    close_parenthesis = clue.get_text().find(")") + 2
-    text = clue.get_text()[close_parenthesis:]
-    clues["down"].append([value, text])
 
-# Extract puzzle structure
-# ========================
+# get the puzzle layout
+def get_layout(width, height, data):
+    blank_puzzle = make_blank_puzzle(15, 15)
 
-size = [15, 15]
-row_data = soup.find("div", class_="crossword__accessible-data")
+    # fill in across answers to solution
+    # across first
+    for clue in data["entries"]:
+        x = clue["position"]["x"]
+        y = clue["position"]["y"]
+        length = clue["length"]
+        solution = clue["solution"]
+        number = clue["number"]
+        blank_puzzle[y][x] = number
+        if clue["direction"] == "across":
+            blank_puzzle[y][x + 1 : x + length] = [0] * (length - 1)
 
-row_data.find_all("li", class_="crossword__accessible-row-data")[0].get_text()
+    # down next
+    for clue in data["entries"]:
+        x = clue["position"]["x"]
+        y = clue["position"]["y"]
+        length = clue["length"]
+        solution = clue["solution"]
+        number = clue["number"]
+        blank_puzzle[y][x] = number
+        if clue["direction"] == "down":
+            for index, row in enumerate(range(y + 1, y + length - 1)):
+                if blank_puzzle[row][x] == "#":
+                    blank_puzzle[row][x] = 0
 
-row_data
+    return blank_puzzle
+
+
+# get the clues from the entry
+def get_clues(data):
+
+    clues = dict()
+    clues["Across"] = []
+    clues["Down"] = []
+
+    for clue in data["entries"]:
+
+        number = clue["number"]
+        text = clue["clue"]
+        direction = clue["direction"]
+
+        clues[direction.capitalize()].append([number, text])
+
+    return clues
+
+
+def get_guardian_puzzle(URL, filepath=None):
+
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    js_crossword = soup.find("div", class_="js-crossword")
+    data = json.loads(js_crossword.get_attribute_list("data-crossword-data")[0])
+
+    puzzle = dict()
+    puzzle["origin"] = f"The Guardian"
+    puzzle["version"] = "http://ipuz.org/v2"
+    puzzle["kind"] = ["http://ipuz.org/crossword"]
+    puzzle["copyright"] = f"{dt.year} Guardian News & Media Limited"
+    puzzle["author"] = data["creator"]["name"]
+    puzzle["publisher"] = "The Guardian"
+    puzzle["url"] = URL
+    puzzle["title"] = data["name"]
+    dt = datetime.datetime.fromtimestamp(data["date"] / 1000)
+    puzzle["date"] = dt.strftime("%m/%d/%Y")
+    puzzle["annotation"] = f"Puzzle type: {data['crosswordType']}"
+    puzzle["dimensions"] = dict(
+        width=data["dimensions"]["cols"], height=data["dimensions"]["rows"]
+    )
+
+    puzzle["puzzle"] = get_layout(15, 15, data)
+    puzzle["clues"] = get_clues(data)
+    puzzle["solution"] = get_solution(15, 15, data)
+
+    output = ipuz.write(puzzle)
+
+    filename = f"Guardian_{data['crosswordType']}_{data['number']}.ipuz"
+
+    if not filepath:
+        filepath = "."
+
+    with open(f"{filepath}/filename", "w") as outfile:
+        json.dump(output)
