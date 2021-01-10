162965b393e36a3e4882351a075db2f6eae6bbb0
Jarred Green <astrojarred@gmail.com>
2021-01-10 02:16:19 +0100

    Try redirection.

--- static/js/app.js
+++ static/js/app.js
@@ -3,13 +3,19 @@ function handleDownloadClick() {
   const query = d3.select('#search_query').property("value");
 
   if (query) {
+    console.log(query)
+    document.getElementById("search_query").placeholder=query;
+
     // base url
-    var baseURL = "https://th6n0jtotb.execute-api.eu-south-1.amazonaws.com/dev/guardian?download=True&puzzle_url=";
+    var baseURL = "https://th6n0jtotb.execute-api.eu-south-1.amazonaws.com/dev/guardian?puzzle_url=";
     var downloadURL = baseURL + query;
-    fetch(downloadURL);
+
+    // Simulate an HTTP redirect:
+    window.location.replace(downloadURL);
 
   };
 }
+
 
 // Attach an event to listen for the search recipes button
 d3.select("#search-btn").on("click", handleDownloadClick);
