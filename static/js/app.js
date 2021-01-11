becae9c4835de949b47677255b3383efb9e8f46f
Jarred Green <astrojarred@gmail.com>
2021-01-11 18:19:18 +0100

    Change js variable names again.

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,10 @@
 // function to handle when clicking the Download button
 function handleDownloadClick() {
-  const query = d3.select('#puzzle-url').property("value");
+  const query = d3.select('#puzzle_url').property("value");
 
   if (query) {
     console.log(query)
-    document.getElementById("puzzle-url").placeholder=query;
+    document.getElementById("puzzle_url").placeholder=query;
 
     // base url
     var baseURL = "https://th6n0jtotb.execute-api.eu-south-1.amazonaws.com/dev/guardian?download=True&puzzle_url=";
