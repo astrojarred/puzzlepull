9e8216b0c9310ed3ea4adabb74dbc6fbb37237e2
Jarred Green <astrojarred@gmail.com>
2024-06-20 19:20:51 +0200

    Add some frontend

---
+++ svelte/src/routes/getPuzzle/+server.ts
@@ -0,0 +1,15 @@
+import { env } from '$env/dynamic/private';
+import { json } from '@sveltejs/kit';
+
+if (!env.API_URL) {
+	throw new Error('API_URL is not defined');
+}
+
+/** @type {import('./$types').RequestHandler} */
+export async function POST({ request }) {
+	// download puzzle from API
+	const { url } = await request.json();
+	const response = await fetch(`${env.API_URL}/guardian?url=${url}?download=true`);
+	const data = await response.json();
+	return json(data);
+}
