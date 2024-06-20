30dfdb1c5fa3195eb233e91fed3d0ffe502422d5
Jarred Green <astrojarred@gmail.com>
2024-06-21 01:57:09 +0200

    Ignore lack of API_URL during build

--- svelte/src/routes/getPuzzle/+server.ts
+++ svelte/src/routes/getPuzzle/+server.ts
@@ -1,17 +1,22 @@
 import { env } from '$env/dynamic/private';
 import { json } from '@sveltejs/kit';
+import { building } from '$app/environment';
 
 let API_URL = env?.API_URL;
-if (!API_URL) {
+if (!API_URL || building) {
 	API_URL = "http://localhost:8000";
 	console.log("WARNING: API_URL not found in environment variables, using default value: http://localhost:8000")
+} else {
+	if (!API_URL) {
+		throw new Error("API_URL not found in environment variables");
+	}
 }
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
 	// download puzzle from API
 	const { url } = await request.json();
-	const response = await fetch(`${env.API_URL}/guardian?url=${url}?download=true`);
+	const response = await fetch(`${API_URL}/guardian?url=${url}?download=true`);
 	const data = await response.json();
 	return json(data);
 }
