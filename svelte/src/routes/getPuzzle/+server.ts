45ffca1adc5fb7c174fdc4884a5f9751fe21f0ab
Jarred Green <astrojarred@gmail.com>
2024-06-21 01:42:33 +0200

    Try using dynamic private env

--- svelte/src/routes/getPuzzle/+server.ts
+++ svelte/src/routes/getPuzzle/+server.ts
@@ -1,7 +1,7 @@
-import { API_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import { json } from '@sveltejs/kit';
 
-if (!API_URL) {
+if (!env?.API_URL) {
 	throw new Error('API_URL is not defined');
 }
 
@@ -9,7 +9,7 @@ if (!API_URL) {
 export async function POST({ request }) {
 	// download puzzle from API
 	const { url } = await request.json();
-	const response = await fetch(`${API_URL}/guardian?url=${url}?download=true`);
+	const response = await fetch(`${env.API_URL}/guardian?url=${url}?download=true`);
 	const data = await response.json();
 	return json(data);
 }
